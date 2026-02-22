a230d7a3bf6e84ddb30f3a0f5f3ca2cf621b55eb
david7482 <david74.chou@gmail.com>
2026-02-22 10:32:03 +0800

    feat: wire LineNotificationSender into worker handler

--- src/spend_tracking/worker/handler.py
+++ src/spend_tracking/worker/handler.py
@@ -2,8 +2,13 @@ import json
 import logging
 import os
 
+import boto3
+
 from spend_tracking.shared.adapters.email_repository_db import DbEmailRepository
 from spend_tracking.shared.adapters.email_storage_s3 import S3EmailStorage
+from spend_tracking.shared.adapters.notification_sender_line import (
+    LineNotificationSender,
+)
 from spend_tracking.shared.adapters.transaction_repository_db import (
     DbTransactionRepository,
 )
@@ -16,7 +21,19 @@ _repository = DbEmailRepository(os.environ["SSM_DB_CONNECTION_STRING"])
 _transaction_repository = DbTransactionRepository(
     os.environ["SSM_DB_CONNECTION_STRING"]
 )
-_service = ProcessEmail(_storage, _repository, _transaction_repository)
+
+_notification_sender = None
+_line_token_param = os.environ.get("SSM_LINE_CHANNEL_ACCESS_TOKEN")
+if _line_token_param:
+    ssm = boto3.client("ssm")
+    response = ssm.get_parameter(Name=_line_token_param, WithDecryption=True)
+    _notification_sender = LineNotificationSender(
+        channel_access_token=response["Parameter"]["Value"]
+    )
+
+_service = ProcessEmail(
+    _storage, _repository, _transaction_repository, _notification_sender
+)
 
 
 def handler(event: dict, context: object) -> None:
